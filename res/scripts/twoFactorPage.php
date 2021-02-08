72f6da1be79676ad0ad4c88dd2274dd5c2f2c223
MrLiveStudio <60267098+mrlivestudio@users.noreply.github.com>
2021-02-08 11:30:34 +0000

    Delete res directory

--- res/scripts/twoFactorPage.php
+++
@@ -1,9 +0,0 @@
-<html>
-<h1>Please enter in your Two-Factor authentication code</h1>
-<p>A two-factor authentication code was emailed to the email cooresponding to this account.  Please find that code and enter it into the following form:</p>
-
-<form method = "POST" action="login.php" enctype="multipart/form-data">
-    <input id="twoFactorBox" type="text" name="twoFactorBox" placeholder="Enter in your 5 digit 2FA code.">
-    <input id="button" name="Submit" type="submit" value="Submit">
-</form>
-</html>
